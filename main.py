98a0a4a53699ec13de9c62bb1418731895f74ac3
sm000ke <mishadymov2005@yandex.ru>
2023-01-30 22:37:56 +1000

    2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-day = int(input('Введите цифру от 0 до 6:'))
+day = int(input('Введите цифру от 1 до 7:'))
 
-if day == 0 or day == 6:
+if day == 6 or day == 7:
     print('Выходной день')
 else:
     print('Не выходной день')
