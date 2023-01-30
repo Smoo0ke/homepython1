beb917cd5bad24813087168480ff1f8c0b533669
Михаил <74896319+Smoo0ke@users.noreply.github.com>
2023-01-30 22:43:20 +1000

    Update 2.py
    Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.

--- 2.py
+++ 2.py
@@ -1,3 +1,7 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+
 x, y, z = map(int, input().split())
 
 if not (x or y or z) == (not x and not y and not z):
